c23e9f3e457544391bd582545280646748045ef0
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-05-28 12:13:24 -0300

    feat: add First Section icons

---
+++ components/Icons/ArrowDownIcon.tsx
@@ -0,0 +1,9 @@
+const ArrowDownIcon = (props: React.SVGProps<SVGSVGElement>) =>{
+    return (
+        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M21.9142 13.2475L22.1616 13L21.9142 12.7525L20.3867 11.225L20.1394 10.9777L19.8919 11.2248L14.4333 16.6736V4.33331V3.98331H14.0833H11.9167H11.5667V4.33331V16.6714L6.11937 11.2144L5.87188 10.9665L5.62417 11.2142L4.08584 12.7525L3.83835 13L4.08584 13.2475L12.7525 21.9141L13 22.1616L13.2475 21.9141L21.9142 13.2475Z" fill="currentColor" stroke="currentColor" strokeWidth="0.7"/>
+        </svg>
+    )
+}
+
+export default ArrowDownIcon
---
+++ components/Icons/AsteriskIcon.tsx
@@ -0,0 +1,9 @@
+const AsteriskIcon = (props: React.SVGProps<SVGSVGElement>) =>{
+    return (
+        <svg width="352" height="610" viewBox="0 0 352 610" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M352 221.487L157.45 327.741V282.845L352 387.603L265.201 531.27L85.6167 423.52L108.065 410.051L112.554 609.09H-58.0508L-53.5612 410.051L-31.1132 423.52L-210.697 531.27L-296 387.603L-101.45 282.845V327.741L-296 221.487L-210.697 77.8199L-31.1132 187.067L-53.5612 200.536L-58.0508 0H112.554L108.065 200.536L85.6167 187.067L265.201 77.8199L352 221.487Z" fill="currentColor"/>
+        </svg>
+    )
+}
+
+export default AsteriskIcon
