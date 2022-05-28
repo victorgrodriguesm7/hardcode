bbd78230ae9d7ed0cefe95ffe4343a351f1c9b16
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-05-28 11:03:05 -0300

    feat: add Header component on mainpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <Header/>
+    </>
   )
 }
 
