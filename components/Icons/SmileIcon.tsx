2afdb93b57dd4d47b63c0134aa3aad33d102c6de
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-06-03 12:24:26 -0300

    feat: create Smile Icon

---
+++ components/Icons/SmileIcon.tsx
@@ -0,0 +1,11 @@
+const SmileIcon = (props: React.SVGProps<SVGSVGElement>) =>{
+    return (
+        <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M16 2.4C16 3.728 14.928 4.8 13.6 4.8C12.272 4.8 11.2 3.728 11.2 2.4C11.2 1.072 12.272 0 13.6 0C14.928 0 16 1.072 16 2.4Z" fill="currentColor"/>
+            <path d="M4.8 2.4C4.8 3.728 3.728 4.8 2.4 4.8C1.072 4.8 0 3.728 0 2.4C0 1.072 1.072 0 2.4 0C3.728 0 4.8 1.072 4.8 2.4Z" fill="currentColor"/>
+            <path d="M16 8.53345C14.7476 12.2637 11.6477 14.9334 8 14.9334C4.35225 14.9334 1.25245 12.2637 0 8.53345H16Z" fill="currentColor"/>
+        </svg>
+    )
+}
+
+export default SmileIcon
